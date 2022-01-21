e04d61430a56ffa19095e70b24e75c94d35ba7cd
Slavik-os <thespacebirds@gmail.com>
2022-01-21 09:57:35 +0100

    1

--- js.js
+++ js.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 
 
 // Sort table
-var properties = [
+var properties =[           // select all th from table to click
   'ID',
   'Brand',
   'Thumnail',
@@ -33,36 +33,36 @@ var properties = [
   'Supply_Date'
 ];
 
-$.each( properties, function( i, val ) {
+$.each( properties, function( i, val ) { // for each elemen in th list
   var orderClass = '';
 
   $("#" + val).click(function(e){
       e.preventDefault();
-      $('.filter__link.filter__link--active').not(this).removeClass('filter__link--active');
+      $('.filter__link.filter__link--active').not(this).removeClass('filter__link--active'); // insure that the columns doesnt contain only nums 
         $(this).toggleClass('filter__link--active');
-         $('.filter__link').removeClass('asc desc');
+         $('.filter__link').removeClass('asc desc'); // keep track if the sorting is asciding or descending
 
-         if(orderClass == 'desc' || orderClass == '') {
-              $(this).addClass('asc');
+         if(orderClass == 'desc' || orderClass == '') { // if sorting is --
+              $(this).addClass('asc'); // add this class of ascending ++ 
               orderClass = 'asc';
          } else {
-             $(this).addClass('desc');
+             $(this).addClass('desc'); // else --
              orderClass = 'desc';
          }
 
-      var parent = $(this).closest('.header__item');
-      var index = $(".header__item").index(parent);
+      var parent = $(this).closest('.header__item'); // Get the child elements of tbody
+      var index = $(".header__item").index(parent); // get index number of parrent (TH)
       var $table = $('.table-content');
       var rows = $table.find('.table-row').get();
-      var isSelected = $(this).hasClass('filter__link--active');
-      var isNumber = $(this).hasClass('filter__link--number');
+      var isSelected = $(this).hasClass('filter__link--active'); // sort by string
+      var isNumber = $(this).hasClass('filter__link--number'); // sort by number
           
       rows.sort(function(a, b){
 
-          var x = $(a).find('.table-data').eq(index).text();
-          var y = $(b).find('.table-data').eq(index).text();
+          var x = $(a).find('.table-data').eq(index).text(); // get text of td
+          var y = $(b).find('.table-data').eq(index).text(); // get text of td to compare
               
-          if(isNumber == true) {
+          if(isNumber == true) { // if the TD is number
                       
               if(isSelected) {
                   return x - y;
@@ -72,7 +72,7 @@ $.each( properties, function( i, val ) {
 
           } else {
           
-              if(isSelected) {		
+              if(isSelected) {		 // if the TD is string
                   if(x < y) return -1;
                   if(x > y) return 1;
                   return 0;
@@ -85,10 +85,10 @@ $.each( properties, function( i, val ) {
           });
 
       $.each(rows, function(index,row) {
-          $table.append(row);
+          $table.append(row); // append each row back to the table again
       });
 
-      return false;
+      return false; // if something went wrong return nothing . 
   });
 
 });
